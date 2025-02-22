611092850698fe323c7015a1bdf80ecd9b752882
Riquelme Gomez <rgomezm13@miumg.edu.gt>
2025-02-21 19:25:33 -0600

    Inicio de Commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        int opcion;
+        System.out.println("\n1. Agregar cliente");
+            System.out.println("2. Atender cliente");
+            System.out.println("3. Mostrar cola de espera");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine(); // Consumir nueva línea
+
+            switch (opcion) {
+                case 1:
+                    // Mensaje
+                    System.out.print("Bienvenido al apartado de Agregar Cliente: ");
+                }
+
+        
+    }
+}
